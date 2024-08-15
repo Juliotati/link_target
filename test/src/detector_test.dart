1c02dd6fbc27a10feb06135d24b4064d60dbdc29
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-08-15 14:37:42 +0100

    Update detector_test.dart

--- test/src/detector_test.dart
+++ test/src/detector_test.dart
@@ -4,19 +4,19 @@ import 'package:link_target/src/detector.dart';
 
 void main() {
   testWidgets(
-    'LinkTargetDetector should be build correctly',
+    'LinkTargetDetector must layout MouseRegion and child',
     (tester) async {
       await tester.pumpWidget(
         const LinkTargetDetector(
           target: 'https://example.com',
           child: Text(
-            'Click here',
+            'Flutter web rocks',
             textDirection: TextDirection.ltr,
           ),
         ),
       );
 
-      expect(find.text('Click here'), findsOneWidget);
+      expect(find.text('Flutter web rocks'), findsOneWidget);
       expect(find.byType(MouseRegion), findsOneWidget);
     },
   );
