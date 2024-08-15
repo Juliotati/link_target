dc9e56d0c70df011db8f112fca0808a46a05ec04
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-08-15 17:21:56 +0100

    feat: allow null target (#2)
    Accepts null targets but quickly returns to minimize layouts

--- test/src/detector_test.dart
+++ test/src/detector_test.dart
@@ -3,9 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:link_target/src/detector.dart';
 
 void main() {
-  testWidgets(
-    'LinkTargetDetector must layout MouseRegion and child',
-    (tester) async {
+  group('LinkTargetDetector', () {
+    testWidgets('should layout MouseRegion and child', (tester) async {
       await tester.pumpWidget(
         const LinkTargetDetector(
           target: 'https://example.com',
@@ -18,6 +17,51 @@ void main() {
 
       expect(find.text('Flutter web rocks'), findsOneWidget);
       expect(find.byType(MouseRegion), findsOneWidget);
-    },
-  );
+    });
+
+    testWidgets('should ignore empty target', (tester) async {
+      await tester.pumpWidget(
+        const LinkTargetDetector(
+          target: '',
+          child: Text(
+            'Flutter web rocks',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      );
+
+      expect(find.text('Flutter web rocks'), findsOneWidget);
+      expect(find.byType(MouseRegion), findsNothing);
+    });
+
+    testWidgets('should ignore "null" string target', (tester) async {
+      await tester.pumpWidget(
+        const LinkTargetDetector(
+          target: 'Null',
+          child: Text(
+            'Flutter web rocks',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      );
+
+      expect(find.text('Flutter web rocks'), findsOneWidget);
+      expect(find.byType(MouseRegion), findsNothing);
+    });
+
+    testWidgets('should ignore null target', (tester) async {
+      await tester.pumpWidget(
+        const LinkTargetDetector(
+          target: null,
+          child: Text(
+            'Flutter web rocks',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      );
+
+      expect(find.text('Flutter web rocks'), findsOneWidget);
+      expect(find.byType(MouseRegion), findsNothing);
+    });
+  });
 }
